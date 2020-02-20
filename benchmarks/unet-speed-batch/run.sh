37e7b95a994f5a9e3f48ecdab6c355942404791e
vibhatha <vibhatha@gmail.com>
2020-02-19 19:21:20 -0500

    adding custom experiment initial configs for variable microbatches

---
+++ benchmarks/unet-speed-batch/run.sh
@@ -0,0 +1,7 @@
+python3 main.py baseline >> baseline.txt
+python3 main.py pipeline-1 >> pipeline-1.txt
+python3 main.py pipeline-2 >> pipeline-2.txt
+python3 main.py pipeline-4 >> pipeline-4.txt
+python3 main.py pipeline-8 >> pipeline-8.txt
+
+
